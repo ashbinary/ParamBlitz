989bd6ba8c81c2e3f7875caee6da84f72df16eb9
ash <111416629+ashbinary@users.noreply.github.com>
2025-04-01 01:41:59 -0700

    Create repository.

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -1,30 +1,102 @@
 #include "lib.hpp"
+#include "loggers.hpp"
+#include <nn.hpp>
+#include <stdio.h>
+#include <iostream>
 
-/* Define hook StubCopyright. Trampoline indicates the original function should be kept. */
-/* HOOK_DEFINE_REPLACE can be used if the original function does not need to be kept. */
-HOOK_DEFINE_TRAMPOLINE(StubCopyright) {
+nn::fs::FileHandle hashList;
+s64 position = 0;
 
-    /* Define the callback for when the function is called. Don't forget to make it static and name it Callback. */
-    static void Callback(bool enabled) {
+namespace sead {
+    template<typename T>
+    struct SafeStringBase {
+        const T* mStringTop;
 
-        /* Call the original function, with the argument always being false. */
-        Orig(false);
-    }
+        virtual ~SafeStringBase() = default;
+        virtual SafeStringBase& operator=(const SafeStringBase& other);
+        virtual void assureTerminationImpl_() const {}
+                
+        SafeStringBase() : mStringTop("") {}
+        SafeStringBase(const T* str) : mStringTop(str) {}
+    };
+    template<typename T>
+    struct BufferedSafeStringBase : public SafeStringBase<T> {
+        uint mLength;
 
-};
+        BufferedSafeStringBase(T* buffer, size_t size) : SafeStringBase<T>(buffer), mLength(size) {}
+        BufferedSafeStringBase(const BufferedSafeStringBase&) = default;
+        ~BufferedSafeStringBase() override = default;
+        BufferedSafeStringBase<T>& operator=(const SafeStringBase<T>& other) override;
+    };
+    using SafeString = SafeStringBase<char>;
+    using BufferedSafeString = BufferedSafeStringBase<char>;
 
+    template <typename T, size_t Size>
+    class FixedSafeStringBase : public BufferedSafeStringBase<T>
+    {
+        public:
+            FixedSafeStringBase() : BufferedSafeStringBase<T>(mBuffer, Size) {}
+            ~FixedSafeStringBase() override = default;
+            FixedSafeStringBase& operator=(const SafeStringBase<T>& other) override;
 
-/* Declare function to dynamic link with. */
-namespace nn::oe {
-    void SetCopyrightVisibility(bool);
+        T mBuffer[Size];
+    };
+
+    template <size_t Size>
+    class FixedSafeString : public FixedSafeStringBase<char, Size>
+    {
+        public:
+            FixedSafeString() : FixedSafeStringBase<char, Size>() {}
+            FixedSafeString(const SafeString& str) : FixedSafeStringBase<char, Size>(str) {}
+
+            FixedSafeString<Size>& operator=(const SafeStringBase<char>& other) override;
+    };
+}
+
+namespace Lp {
+    namespace Sys {
+        struct ParamNode {
+            u64 field_8;
+            const char *mParamName;
+            uintptr_t *mNext;
+            uintptr_t *mParent;
+            u32 mFlags;
+            u32 field_2C;
+
+            uint keyHash() const;  
+            virtual ~ParamNode();
+            virtual void key(sead::BufferedSafeString*) const;
+        };
+    };
+}
+
+HOOK_DEFINE_INLINE(SaveBuffer) {
+    static void Callback(exl::hook::InlineCtx* context)
+    {
+        sead::FixedSafeString<256>* bufferPtr = reinterpret_cast<sead::FixedSafeString<256>*>(context->X[0]);
+        const size_t strSize = strlen(bufferPtr->mStringTop);
+
+        nn::fs::WriteFile(hashList, position, bufferPtr->mStringTop, strSize, 
+            nn::fs::WriteOption::CreateOption(nn::fs::WriteOptionFlag_Flush));
+        position += strSize;
+
+        nn::fs::WriteFile(hashList, position, "\n", strlen("\n"), 
+            nn::fs::WriteOption::CreateOption(nn::fs::WriteOptionFlag_Flush));
+        position += strlen("\n");
+    };
 };
 
 extern "C" void exl_main(void* x0, void* x1) {
     /* Setup hooking environment. */
     exl::hook::Initialize();
 
+    nn::fs::MountSdCardForDebug("sd");
+    nn::fs::CreateFile("sd:/bprmList.txt", 0);
+    nn::fs::OpenFile(&hashList, "sd:/bprmList.txt", nn::fs::OpenMode_ReadWrite | nn::fs::OpenMode_Append);
+
     /* Install the hook at the provided function pointer. Function type is checked against the callback function. */
-    StubCopyright::InstallAtFuncPtr(nn::oe::SetCopyrightVisibility);
+    //GetParamData::InstallAtFuncPtr(&Lp::Sys::ParamNode::keyHash);
+    SaveBuffer::InstallAtOffset(0x019D1000); // At hashing function
 
     /* Alternative install funcs: */
     /* InstallAtPtr takes an absolute address as a uintptr_t. */
